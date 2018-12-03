27c983dea834d98d4f7fdc9a145471818dd05038
Davidparnell <c15704085@mydit.ie>
2018-12-03 16:22:19 +0000

    Add files via upload

--- Home.php
+++ Home.php
@@ -47,37 +47,14 @@ adding more features and components -->
 	<title>Vape Website</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="vapeShop.css">
+	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	
-	<style>
-		/* Add a gray background color and some padding to the footer */
-	header {
-      background-color: #212121;
-      padding: 25px;
-    }
-    footer {
-      background-color: #f2f2f2;
-      padding: 25px;
-    }
-		.modal-header, h4, .close {
-		background-color: #101010;
-		color:white !important;
-		text-align: center;
-		font-size: 30px;
-		}
-		.modal-footer {
-			background-color: #101010;
-			color: white;
-		}
-		
-		.btn-block {
-			display: block;
-			width: 100%;
-			background-color: #101010;
-		}
-    </style>
+	
 	<script>
 		$(document).ready(function(){
 			$("#myBtn").click(function(){
@@ -92,8 +69,6 @@ adding more features and components -->
 	<header class="container-fluid text-center">
 		<div class="container-fluid">
 			<a href="Home.php" style="color: white; text-decoration: none"><h1>VAPE SHOP</h1></a>
-			<input type="text" placeholder="Search items..">
-			<a href="#" class="glyphicon glyphicon-search" style="color: white; text-decoration: none"></a>
 		</div>
 	</header>
 	
@@ -114,52 +89,14 @@ adding more features and components -->
 					<li><a href="#">About Us</a></li>
 				</ul>
 				<div class="collapse navbar-collapse" id="myNavbar">
-				<ul class="nav navbar-nav">=
-					<li><a href="#">Contact Us</a></li>
+				<ul class="nav navbar-nav">
+					 
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
-					<li class="active"><a href="Basket.php"><span class="glyphicon glyphicon-shopping-cart"></span> My Cart</a></li>
+					<li><a href="Basket.php"><span class="glyphicon glyphicon-shopping-cart"></span> My Cart</a></li>
 					<li><button type="button" class="btn btn-default btn-lg" id="myBtn"><?php 
 					echo $login; ?></button></li></li>
 					
-					<div class="modal fade" id="myModal" role="dialog">
-						<div class="modal-dialog">
-    
-						<!-- Modal content-->
-						<div class="modal-content">
-						<div class="modal-header" style="padding:35px 50px;">
-							<button type="button" class="close" data-dismiss="modal">&times;</button>
-							<h4><span class="glyphicon glyphicon-lock"></span> Login</h4>
-						</div>
-					
-						<div class="modal-body" style="padding:40px 50px;">
-							<div class="form-group">
-
-							<form role="form" action = "" method = "post">
-								<label for="username"><span class="glyphicon glyphicon-user"></span> Username</label>
-								<input type="text" class="form-control" name="username" placeholder="Enter Username">
-								<label for="password"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
-								<input type="text" class="form-control" name="password" placeholder="Enter password">
-							
-						
-								<div class="checkbox">
-									<label><input type="checkbox" value="" checked>Remember me</label>
-								</div>
-								<button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button>
-								<?php 
-									echo $errormsg; 
-								?>
-								</form>
-							</div>
-						</div>
-					
-						<div class="modal-footer">
-							<button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
-							<p>Not a member? <a href="Register.php">Sign Up</a></p>
-							<p>Forgot <a href="#">Password?</a></p>
-						</div>
-						</div>
-					</div>
 					</div>
 				</ul>
 			</div>
@@ -252,7 +189,7 @@ adding more features and components -->
 	</div>
 
 	<div class="container text-center" id="part2">
-		<h1>What is Vaping?</h1>
+		<h1 style="color:black">What is Vaping?</h1>
 		<br>
 		<div class="row">
 			<div class="col-sm-5">
